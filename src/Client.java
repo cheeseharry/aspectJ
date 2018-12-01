3e83cf58dd091f3a9927d27185e6367902a0719d
lei zheng <zhenglei.cs04@g2.nctu.edu.tw>
2018-12-01 00:05:27 -0600

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,22 @@
+public class Client
+{
+
+ public static void main(String[] args) {
+ 
+      Account acc1, acc2;
+      
+      try { 
+            MoneyTransferService service = new MoneyTransferService();     
+            acc1=new Account("Gulustan", 12345, 400);
+            acc2=new Account("Brian", 12346, 375);
+            service.moneyTransfer(acc1, acc2, 200);
+      } 
+      catch(Exception e) 
+      {
+        e.printStackTrace();
+      }
+   }
+
+   
+}
+
